76eadc969e7b83c823c4226717f12fe4d497bdfb
Jason Sato <jason.sato@slalom.com>
2020-04-03 16:59:26 -0700

    subpath behavior

--- hello.py
+++ hello.py
@@ -20,3 +20,8 @@ def show_user_profile(username):
 def show_post(post_id):
     #show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    #show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
