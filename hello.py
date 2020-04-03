1d210869863b1fbe13bf70ee53e9c3e52cde8319
Jason Sato <jason.sato@slalom.com>
2020-04-03 16:56:04 -0700

    username substitution route

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +10,8 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #show the user profile for user
+    return 'User %s' % escape(username)
