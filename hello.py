5e0bc920afba18b973d6fb48953be6fc1d3e8493
Jason Sato <jason.sato@slalom.com>
2020-04-03 16:57:28 -0700

    int substitution route

--- hello.py
+++ hello.py
@@ -15,3 +15,8 @@ def hello():
 def show_user_profile(username):
     #show the user profile for user
     return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    #show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
