22c1cfad6ee8726b9f847d28c7c7e68d65e4c5e6
Jason Sato <jsato03@gmail.com>
2020-04-03 17:33:32 -0700

    formatting

--- hello.py
+++ hello.py
@@ -1,35 +1,42 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Index Page'
+    return "Index Page"
 
-@app.route('/hello')
+
+@app.route("/hello")
 def hello():
-    return 'Hello, World'
+    return "Hello, World"
 
-@app.route('/user/<username>')
+
+@app.route("/user/<username>")
 def show_user_profile(username):
-    #show the user profile for user
-    return 'User %s' % escape(username)
+    # show the user profile for user
+    return "User %s" % escape(username)
 
-@app.route('/post/<int:post_id>')
+
+@app.route("/post/<int:post_id>")
 def show_post(post_id):
-    #show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+    # show the post with the given id, the id is an integer
+    return "Post %d" % post_id
 
-@app.route('/path/<path:subpath>')
+
+@app.route("/path/<path:subpath>")
 def show_subpath(subpath):
-    #show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
+    # show the subpath after /path/
+    return "Subpath %s" % escape(subpath)
 
-@app.route('/projects/')
+
+@app.route("/projects/")
 def projects():
-    return 'The project page'
+    return "The project page"
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return 'The about page'
+    return "The about page"
